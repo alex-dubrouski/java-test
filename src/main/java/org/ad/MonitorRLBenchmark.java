0cb5334e31c2614200d4e975f4fc1ce8fdafa790
Aliaksei Dubrouski <adubrouski@pandora.com>
2020-02-25 09:45:08 -0800

    MOre options

--- src/main/java/org/ad/MonitorRLBenchmark.java
+++ src/main/java/org/ad/MonitorRLBenchmark.java
@@ -1,10 +1,10 @@
 package org.ad;
 
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.CompilerControl;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Group;
 import org.openjdk.jmh.annotations.GroupThreads;
@@ -24,7 +24,7 @@ import org.openjdk.jmh.infra.Blackhole;
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
-@Fork(value = 1, jvmArgsAppend = {"-XX:+UnlockExperimentalVMOptions", "-XX:+UseEpsilonGC", "-XX:-UseBiasedLocking", "-XX:+AlwaysPreTouch", "-Xms2g", "-Xmx2g"})
+@Fork(value = 1, jvmArgsAppend = {"-XX:+UnlockExperimentalVMOptions", "-XX:+UseEpsilonGC", "-XX:-UseBiasedLocking", "-XX:+UnlockDiagnosticVMOptions", "-XX:-DoEscapeAnalysis", "-XX:+AlwaysPreTouch", "-XX:LoopUnrollLimit=1", "-Xms2g", "-Xmx2g"})
 @Threads(8)
 public class MonitorRLBenchmark {
   final Object mutex = new Object();
@@ -38,10 +38,11 @@ public class MonitorRLBenchmark {
   @Benchmark
   @Group("monitor")
   @GroupThreads(8)
+  @CompilerControl(CompilerControl.Mode.DONT_INLINE)
   public void lockOnMonitor(Blackhole bh) {
     for(int i = 0; i < 25; i++) {
       synchronized (mutex) {
-        bh.consume(ThreadLocalRandom.current().nextInt());
+        bh.consume(0x42);
       }
     }
   }
@@ -49,11 +50,15 @@ public class MonitorRLBenchmark {
   @Benchmark
   @Group("relock")
   @GroupThreads(8)
+  @CompilerControl(CompilerControl.Mode.DONT_INLINE)
   public void lockOnReLock(Blackhole bh) {
     for(int i = 0; i < 25; i++) {
       lock.lock();
-      bh.consume(ThreadLocalRandom.current().nextInt());
-      lock.unlock();
+      try {
+        bh.consume(0x42);
+      } finally {
+        lock.unlock();
+      }
     }
   }
 }
